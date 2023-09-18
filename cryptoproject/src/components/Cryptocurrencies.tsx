a366f6a1207131e1691f0125b54442b6e49d6085
Umut Can Yilmaz <ylmazumutcan49@gmail.com>
2023-09-18 20:07:19 +0300

    InputHalledildi

--- cryptoproject/src/components/Cryptocurrencies.tsx
+++ cryptoproject/src/components/Cryptocurrencies.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import millify from "millify";
 import { Link } from "react-router-dom";
 import { Card, Row, Col, Input } from "antd";
@@ -11,14 +11,27 @@ interface Props {
 const Cryptocurrencies: React.FC<Props> = ({ simplified }) => {
   const count = simplified ? 10 : 100;
   const { data: cryptosList, isFetching } = useGetCryptosQuery(count);
-  const [cryptos, setCryptos] = useState(cryptosList?.data?.coins);
+  const [cryptos, setCryptos] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  useEffect(() => {
+    const filteredData = cryptosList?.data?.coins.filter((coin: any) =>
+      coin.name.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+
+    setCryptos(filteredData);
+  }, [cryptosList, searchTerm]);
 
   if (isFetching) return "Loading...";
 
-  console.log(cryptos);
-
   return (
     <div className="yazi">
+      <div className="search-crypto">
+        <Input
+          placeholder="İstediğiniz kripto parayı yazınız."
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+      </div>
       <Row gutter={[32, 32]} className="crypto-card-container  ">
         {cryptos?.map((currency: any) => (
           <Col
